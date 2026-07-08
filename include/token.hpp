471270108be5312b4b0caa80903aa1603a055ea5
Brandon McConathy <brandonmcconathy4321@gmail.com>
2026-07-08 10:43:04 -1000

    built token namespace

---
+++ include/token.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <string>
+
+namespace Token {
+
+	using TokenType = std::string;
+
+	struct Token {
+		TokenType	Type;
+		std::string	Literal;
+	};
+
+	// Define possible TokenTypes
+	// Special
+	inline constexpr std::string ILLEGAL 	= "ILLEGAL";
+	inline constexpr std::string END		= "END";
+
+	// Identifiers and literals
+	inline constexpr std::string IDENT 		= "IDENT";
+	inline constexpr std::string INT 		= "INT";
+
+	// Operators
+	inline constexpr std::string ASSIGN 	= "ASSIGN";
+	inline constexpr std::string PLUS 		= "+";
+
+	// Delimeters
+	inline constexpr std::string COMMA 		= ",";
+	inline constexpr std::string SEMICOLON 	= ";";
+
+	// Utility
+	inline constexpr std::string LPAREN 	= "(";
+	inline constexpr std::string RPAREN 	= ")";
+	inline constexpr std::string LBRACE 	= "{";
+	inline constexpr std::string RBRACE 	= "}";
+
+	// Keywords
+	inline constexpr std::string FUNCTION 	= "FUNCTION";
+	inline constexpr std::string LET 		= "LET";
+}
