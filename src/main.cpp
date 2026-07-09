c6dda90bfe710769414da320541d348e9575335b
Brandon McConathy <brandonmcconathy4321@gmail.com>
2026-07-08 17:05:37 -1000

    Added lexer

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
 
 #include <token.hpp>
+#include <lexer.hpp>
 
-int main(void) {
-	std::cout << "Hello World" << std::endl;
+int main(int argc, char* argv[]) {
+	// Starts REPL
+	if (argc == 2) {
+		// TODO change condition to (argc == 1) and start REPL
+		return 0;
+	}
+
+	std::string test_string = "+(){};";
+	Lexer l = Lexer(test_string);
+	l.print();
+	while (l.ch != 0) {
+		l.readChar();
+		l.print();
+	}
 	return 0;
 }
